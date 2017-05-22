13ec5a30fe7062f2ed3510b0db62dd2ad9f360a8
Jeff Storck <jtswork@gmail.com>
2017-05-22 14:28:22 -0400

    Updated install-hdf-mpack.sh to use newest HDF 3.0 mpack build Updated messages.properties to use ambari 2.5.0.3 and HDF 3.0

--- orchestrator-salt/src/main/resources/salt/salt/ambari/scripts/install-hdf-mpack.sh
+++ orchestrator-salt/src/main/resources/salt/salt/ambari/scripts/install-hdf-mpack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
-export hdf_ambari_mpack_url="http://public-repo-1.hortonworks.com/HDF/centos7/2.x/updates/2.1.2.0/tars/hdf_ambari_mp/hdf-ambari-mpack-2.1.2.0-10.tar.gz"
+#export hdf_ambari_mpack_url="http://public-repo-1.hortonworks.com/HDF/centos7/2.x/updates/2.1.2.0/tars/hdf_ambari_mp/hdf-ambari-mpack-2.1.2.0-10.tar.gz"
+export hdf_ambari_mpack_url="http://s3.amazonaws.com/dev.hortonworks.com/HDF/centos6/3.x/BUILDS/3.0.0.0-337/tars/hdf_ambari_mp/hdf-ambari-mpack-3.0.0.0-337.tar.gz"
 stop ambari-server
-echo yes | ambari-server install-mpack --mpack=${hdf_ambari_mpack_url} --purge --verbose
+echo yes | ambari-server install-mpack --mpack=${hdf_ambari_mpack_url} --verbose
 start ambari-server
